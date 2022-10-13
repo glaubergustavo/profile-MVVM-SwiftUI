4dc8c7cbdd833829e02569835c2d11bd47654d86
Glauber Gustavo <glauber.filho@madeinweb.app>
2022-10-13 09:51:44 -0300

    Initial Commit

---
+++ Profile/ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileApp.swift
+//  Profile
+//
+//  Created by Glauber Gustavo on 13/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
